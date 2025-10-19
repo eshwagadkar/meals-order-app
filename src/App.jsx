5a5637773bc73b413fe1b4681db9ce56fbe7920c
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-19 19:22:33 +0530

    Fetching Meals Data [GET http request]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
+import Meals from './components/Meals'
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
 
-export default App;
+export default App
